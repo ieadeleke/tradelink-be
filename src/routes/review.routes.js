5600639fb1533db51df7a4754b3303b6a3fabb50
ieadeleke <eadelekeife@yahoo.com>
2025-09-07 16:03:29 +0100

    seller full profile details

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -3,6 +3,9 @@ const { authRequired } = require('../middleware/auth');
 const ctrl = require('../controllers/review.controller');
 
 // Create a new review for a seller
+router.get('/', (req,res) => {
+    res.send('hi there gee')
+});
 router.post('/', authRequired, ctrl.createReview);
 
 module.exports = router;
